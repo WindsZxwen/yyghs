ede88467766e0c39a2a908a8bf2c042fa0205c8d
WindsZxwen <202212490382@nuist.edu.cn>
2023-09-18 17:56:40 +0800

    first commit

---
+++ yygh_parents/src/main/java/com/example/yygh_parents/YyghParentsApplication.java
@@ -0,0 +1,14 @@
+package com.example.yygh_parents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class YyghParentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyghParentsApplication.class, args);
+	}
+
+}
