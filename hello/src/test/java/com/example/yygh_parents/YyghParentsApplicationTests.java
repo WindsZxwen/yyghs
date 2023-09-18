6989da61fcb5032c6e36cc5c5e45fda9bb2f3e0a
WindsZxwen <202212490382@nuist.edu.cn>
2023-09-18 18:01:33 +0800

    first commit

---
+++ hello/src/test/java/com/example/yygh_parents/YyghParentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yygh_parents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
